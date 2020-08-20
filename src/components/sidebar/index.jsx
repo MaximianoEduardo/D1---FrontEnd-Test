2abf3108055d2236fa66c2156eb50c721a166579
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-20 18:25:37 -0300

    🚧 Sidebar

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logoD1 from '../../assets/images/logotipo.png'
+import './index.css'
+
+
+function SideBar(){
+
+
+    return(
+
+        <div className="sideBar">
+
+            <img src={logoD1} alt="Logo D1"/>
+
+
+        </div>
+
+
+
+    )
+
+
+}
+
+export default SideBar
