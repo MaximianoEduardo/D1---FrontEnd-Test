54190faa5b6d79bbc4d22f1fc9643973ba3b3610
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-24 15:01:05 -0300

    Styles Itens Journey ⚙️ Status needs Att 🔴

---
+++ src/components/itemJourney/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+function JourneyItem({ id, name, destinary, status, sucess }){
+  
+    
+    function statusString()
+        {    
+
+            console.log(status)
+
+            switch (status) {
+                case status === 1:
+                    return 'Em execução'
+                case status === 2 :
+                    return 'ativa'
+                case status === 3 :
+                    return 'Configurando'
+                case status === 4 :
+                    return 'Ociosa'
+                case status === 5 :
+                    return 'Concluida'
+                default :
+                    return console.log('ok')
+            }
+
+        }
+    
+    return(
+        
+      
+    
+        <div className='journeyItem'> 
+            
+            <span className='journeyItemName'>
+                {name}
+            </span>
+
+            <span className='journeyItemDestiny'>
+                {destinary}
+            </span>
+
+            <span className='journeyItemSucess'>
+                {sucess}
+            </span>
+
+            <span>
+                {
+                                 
+                    status
+
+                }
+            </span>
+
+        </div>
+
+
+       
+    )
+
+
+}
+
+export default JourneyItem
