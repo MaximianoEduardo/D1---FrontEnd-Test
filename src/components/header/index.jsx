56cd705a4237221a8b861c2efa89435725328b4c
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-08-25 22:04:36 -0300

    Header User Tooltip  💁 Hover Button 📚

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CompanyLogo from '../../assets/images/acme-logo.png'
 import plusIcon from '../../assets/icons/plus.svg'
+import Tooltip from 'react-simple-tooltip'
 import './index.css'
+import Modal from '../modal'
 
 
 function Header(){
 
+    const [ show, setShow ] = useState( false )
 
     return(
 
@@ -13,7 +16,20 @@ function Header(){
 
             <div className="right__header">
 
-                <span className='header__user'> A </span>
+                <Tooltip 
+                    content="Antonio da Silva"
+                    color='#fff'
+                    background='#1A1731'
+                    placement='right'
+                    border='none'
+                    fontSize='13'
+                    padding={9}
+                    radius={7}
+                    arrow={7}
+                >
+                    <span className='header__user'> A </span>
+                </Tooltip>
+                
 
                 <span className="header__company">
                     <img src={CompanyLogo} alt="logo da companhia"/>
@@ -26,8 +42,9 @@ function Header(){
 
                 <input type="text" placeholder='Buscar' />
 
-                <a href='#' className='left__buttonJornada'> 
+                <a href='#' className='left__buttonJornada' id="centered-toggle-button" onClick={ () => (setShow(!show)) } > 
                    <img src={plusIcon} alt=""/>  Nova Jornada
+                   <Modal show={show} />
                 </a>
 
             </div>
